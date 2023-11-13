d696500c578724bd6dd4162cedaf541740476f8c
Casey Capetola <caseycapetola@gmail.com>
2023-11-13 16:39:28 -0500

    Imported logic and initialized Pair class

--- src/CFTP.java
+++ src/CFTP.java
@@ -10,20 +10,114 @@ public class CFTP {
     private static int[][] gridPOS = new int[GRID_SIZE][GRID_SIZE];
     private static int[][] gridNEG = new int[GRID_SIZE][GRID_SIZE];
     private static int diff = GRID_SIZE*GRID_SIZE;
+    
+    
+ // Ising Calculation for POS grid
+  	private static double isingCalculationPos(int x, int y) {
+      	int numPos = 0;
+      	int numNeg = 0;
+      	
+  		// CHECK HORIZONTAL NEIGHBORS
+  		if(y>0) {
+  			if(gridPOS[x][y-1] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		if(y<GRID_SIZE-1) {
+  			if(gridPOS[x][y+1] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		
+  		// CHECK VERTICAL NEIGHBORS
+  		if(x>0) {
+  			if(gridPOS[x-1][y] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		if(x<GRID_SIZE-1) {
+  			if(gridPOS[x+1][y] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		
+  		return (Math.exp(BETA*numPos)/(Math.exp(BETA*numPos) + Math.exp(BETA*numNeg)));
+      	
+      }
+  	
+  	// Ising Calculation for NEG grid
+  	private static double isingCalculationNeg(int x, int y) {
+      	int numPos = 0;
+      	int numNeg = 0;
+      	
+  		// CHECK HORIZONTAL NEIGHBORS
+  		if(y>0) {
+  			if(gridNEG[x][y-1] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		if(y<GRID_SIZE-1) {
+  			if(gridNEG[x][y+1] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		
+  		// CHECK VERTICAL NEIGHBORS
+  		if(x>0) {
+  			if(gridNEG[x-1][y] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		if(x<GRID_SIZE-1) {
+  			if(gridNEG[x+1][y] == POS) {
+  				numPos++;
+  			}
+  			else {
+  				numNeg++;
+  			}
+  		}
+  		
+  		return (Math.exp(BETA*numPos)/(Math.exp(BETA*numPos) + Math.exp(BETA*numNeg)));
+      	
+      }
+    
+    public boolean coupling(int steps) {
+    	return false;
+    }
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// Initialize grids to all positive/negative
-				for(int i=0; i<GRID_SIZE; i++) {
-					for(int j=0; j<GRID_SIZE; j++) {
-						gridPOS[i][j] = POS;
-						gridNEG[i][j] = NEG;
-						
-					}
-				}
+		for(int i=0; i<GRID_SIZE; i++) {
+			for(int j=0; j<GRID_SIZE; j++) {
+				gridPOS[i][j] = POS;
+				gridNEG[i][j] = NEG;
 				
-				int xcoord, ycoord;
-				Random random = new Random();
+			}
+		}
+		
+		int xcoord, ycoord;
+		Random random = new Random();
 	}
 
 }
