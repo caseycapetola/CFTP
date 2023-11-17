a5763ae334360b6cfb27ab3586bebad9f5deae68
Casey Capetola <caseycapetola@gmail.com>
2023-11-17 15:29:37 -0500

    Created Pair class and started coupling logic in CFTP

--- src/CFTP.java
+++ src/CFTP.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class CFTP {
 
-	private static final int GRID_SIZE = 9;
-	private static final double BETA = 1.5;
+	private static final int GRID_SIZE = 10;
+	private static final double BETA = 0.5;
     private static final int POS = 1;
     private static final int NEG = -1;
     
     private static int[][] gridPOS = new int[GRID_SIZE][GRID_SIZE];
     private static int[][] gridNEG = new int[GRID_SIZE][GRID_SIZE];
     private static int diff = GRID_SIZE*GRID_SIZE;
+    private static ArrayList<Pair> moves = new ArrayList<Pair>();
     
     
  // Ising Calculation for POS grid
@@ -102,8 +104,58 @@ public class CFTP {
       	
       }
     
-    public boolean coupling(int steps) {
-    	return false;
+    public static void coupling(double flag, double flipPosPOS, double flipPosNEG, int xcoord, int ycoord) {
+    	// If flag < flipPosPOS/NEG, set both grids to positive
+		if(flag<=flipPosPOS && flag<=flipPosNEG) {
+			// check if the grids agree
+			if (gridPOS[xcoord][ycoord] == gridNEG[xcoord][ycoord]) {
+				gridPOS[xcoord][ycoord] = POS;
+				gridNEG[xcoord][ycoord] = POS;
+			}
+			else {
+				gridPOS[xcoord][ycoord] = POS;
+				gridNEG[xcoord][ycoord] = POS;
+				diff--;
+			}
+		}
+		//If flag > flipPosPOS/NEG, set both grids to negative
+		else if(flag>flipPosPOS && flag>flipPosNEG) {
+			// check if the grids agree
+			if (gridPOS[xcoord][ycoord] == gridNEG[xcoord][ycoord]) {
+				gridPOS[xcoord][ycoord] = NEG;
+				gridNEG[xcoord][ycoord] = NEG;
+			}
+			else {
+				gridPOS[xcoord][ycoord] = NEG;
+				gridNEG[xcoord][ycoord] = NEG;
+				diff--;
+			}
+		}
+		// If they disagree, set opposite
+		else if(flag<=flipPosPOS && flag>flipPosNEG) {
+			// check if the grids agree
+			if (gridPOS[xcoord][ycoord] == gridNEG[xcoord][ycoord]) {
+				gridPOS[xcoord][ycoord] = POS;
+				gridNEG[xcoord][ycoord] = NEG;
+				diff++;
+			}
+			else {
+				gridPOS[xcoord][ycoord] = POS;
+				gridNEG[xcoord][ycoord] = NEG;
+			}
+		}
+		else {
+			// check if the grids agree
+			if (gridPOS[xcoord][ycoord] == gridNEG[xcoord][ycoord]) {
+				gridPOS[xcoord][ycoord] = NEG;
+				gridNEG[xcoord][ycoord] = POS;
+				diff++;
+			}
+			else {
+				gridPOS[xcoord][ycoord] = NEG;
+				gridNEG[xcoord][ycoord] = POS;
+			}
+		}
     }
 	
 	public static void main(String[] args) {
@@ -118,6 +170,37 @@ public class CFTP {
 		
 		int xcoord, ycoord;
 		Random random = new Random();
+		
+		// Each iteration we'll try 100 moves (times 2 for each iteration)
+		int steps = 100;
+		
+		boolean isCoupled = false;
+		
+		while(!isCoupled) {
+			// STEP 1: Iterate over new randomness
+			for(int i=0; i<steps; i++) {
+				xcoord = random.nextInt(GRID_SIZE);
+				ycoord = random.nextInt(GRID_SIZE);
+				double flipPosPOS = isingCalculationPos(xcoord, ycoord);
+				double flipPosNEG = isingCalculationNeg(xcoord, ycoord);
+				double flag = random.nextDouble();
+				moves.add(0, new Pair(xcoord, ycoord, flag, flipPosPOS, flipPosNEG));
+				coupling(flag, flipPosPOS, flipPosNEG, xcoord, ycoord);
+			}
+			
+			// STEP 2: Iterate over old logic
+			int index = moves.size();
+			for(int j=steps; j<moves.size(); j++) {
+				coupling(moves.get(j).randNum, moves.get(j).posCalc, moves.get(j).negCalc, moves.get(j).x, moves.get(j).y);
+			}
+			
+			if(diff==0) {
+				isCoupled = true;
+				
+				// ADD BINARY SEARCH LOGIC TO FIND EXACTLY WHERE WE COUPLE
+			}
+		}
+		System.out.println(moves.size());
 	}
 
 }
--- src/Pair.java
+++ src/Pair.java
@@ -1,4 +1,24 @@
 
 public class Pair {
-
+	int x;
+	int y;
+	double randNum;
+	double posCalc;
+	double negCalc;
+	
+	public Pair(int xcoord, int ycoord, double val, double posCalc, double negCalc) {
+		x = xcoord;
+		y = ycoord;
+		randNum = val;
+		this.posCalc = posCalc;
+		this.negCalc = negCalc;
+	}
+	
+	// Set x and y coordinates for move
+	public void setCoords(int xcoord, int ycoord) {
+		x = xcoord;
+		y = ycoord;
+	}
 }
+
+
