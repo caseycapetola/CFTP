aa73cca0bec60d5ff52f5493032d7dfcd45e21e5
Casey Capetola <caseycapetola@gmail.com>
2023-12-05 14:20:00 -0500

    Added coupling functionalities to IsingGrid

--- src/IsingGrid.java
+++ src/IsingGrid.java
@@ -1,29 +1,21 @@
+import java.util.ArrayList;
 
 public class IsingGrid {
 	private static final int POS = 1;
     private static final int NEG = -1;
+    private static int diff;
 	int[][] layout;
 	int grid_size;
+	int type;
 	double beta;
 	
-	public IsingGrid(int type, int size, double b) {
+	public IsingGrid(int t, int size, double b) {
 		grid_size = size;
 		beta = b;
 		layout = new int[size][size];
-//		if(type==POS) {
-//			for(int i=0; i<size; i++) {
-//				for(int j=0; j<size; j++) {
-//					layout[i][j] = POS;
-//				}
-//			}
-//		} else {
-//			for(int i=0; i<size; i++) {
-//				for(int j=0; j<size; j++) {
-//					layout[i][j] = NEG;
-//				}
-//			}
-//		}
-		resetGrid(size);
+		type = t;
+		resetGrid(type);
+		diff = grid_size*grid_size;
 	}
 	
 	// Conducts Ising Calculation for given layout
@@ -71,6 +63,7 @@ public class IsingGrid {
 	}
 
 	public void resetGrid(int type) {
+		diff = grid_size*grid_size;
 		if(type==POS) {
 			for(int i=0; i<grid_size; i++) {
 				for(int j=0; j<grid_size; j++) {
@@ -84,5 +77,61 @@ public class IsingGrid {
 				}
 			}
 		}
+	}
+
+	private void coupleStep(IsingGrid grid2, double flag, double flip1, double flip2, int x, int y) {
+		if(flag<=flip1 && flag<=flip2) {
+			// check if grids agree
+			if (layout[x][y] == grid2.layout[x][y]) {
+				layout[x][y] = POS;
+				grid2.layout[x][y] = POS;
+			} else {
+				layout[x][y] = POS;
+				grid2.layout[x][y] = POS;
+				diff--;
+			}
+		}
+		else if(flag>flip1 && flag>flip2) {
+			if (layout[x][y] == grid2.layout[x][y]) {
+				layout[x][y] = NEG;
+				grid2.layout[x][y] = NEG;
+			} else {
+				layout[x][y] = NEG;
+				grid2.layout[x][y] = NEG;
+				diff--;
+			}
+		}
+		else if(flag<=flip1 && flag>flip2) {
+			if (layout[x][y] == grid2.layout[x][y]) {
+				layout[x][y] = POS;
+				grid2.layout[x][y] = NEG;
+				diff++;
+			} else {
+				layout[x][y] = POS;
+				grid2.layout[x][y] = NEG;
+			}
+		}
+		else {
+			if (layout[x][y] == grid2.layout[x][y]) {
+				layout[x][y] = NEG;
+				grid2.layout[x][y] = POS;
+				diff++;
+			} else {
+				layout[x][y] = NEG;
+				grid2.layout[x][y] = POS;
+			}
+		}
+	}
+	
+	public int isCoupled(IsingGrid grid2, ArrayList<Pair> moves, int index) {
+		resetGrid(type);
+		grid2.resetGrid(grid2.type);
+		
+		for(int i=index; i<moves.size(); i++) {
+			int x = moves.get(i).x, y = moves.get(i).y;
+			coupleStep(grid2, moves.get(i).randNum, isingCalculation(x, y), grid2.isingCalculation(x, y), x, y);
+		}
+		
+		return diff;
 	}
 }
